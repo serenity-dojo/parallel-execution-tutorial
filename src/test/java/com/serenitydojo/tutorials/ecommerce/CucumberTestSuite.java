a8335ef39fc0d52c94e9d654ec59e0969ae5d211
John Ferguson Smart <john.smart@wakaleo.com>
2023-02-17 17:54:17 +0000

    Parallel Cucumber test execution

--- src/test/java/com/serenitydojo/tutorials/ecommerce/CucumberTestSuite.java
+++ src/test/java/com/serenitydojo/tutorials/ecommerce/CucumberTestSuite.java
@@ -1,12 +1,10 @@
 package com.serenitydojo.tutorials.ecommerce;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        plugin = {"pretty"},
-        features = "src/test/resources/features"
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
 public class CucumberTestSuite {}
